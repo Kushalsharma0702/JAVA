f4667494428345c2ffe5cde3b38918d55c019433
Kushal sharma <146510678+Kushalsharma0702@users.noreply.github.com>
2024-07-22 01:02:37 +0530

    Create Datavariables.java

---
+++ Day-01/Datavariables.java
@@ -0,0 +1,10 @@
+class Datavariables {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        int num1 = 2;
+        int num2 = 3;
+        int num3 = num1+num2;
+        System.out.println(num3);
+    }
+}
+ 
