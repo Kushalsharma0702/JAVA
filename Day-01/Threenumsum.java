84e477459fd8830f0b9c01d54a0f7fb3581fa5ce
Kushal sharma <146510678+Kushalsharma0702@users.noreply.github.com>
2024-07-22 01:13:47 +0530

    Create Threenumsum.java

---
+++ Day-01/Threenumsum.java
@@ -0,0 +1,13 @@
+class Threenumsum {
+    public static void main (String[] args)
+    {
+      int num1=3;
+      int num2=4;
+      int num3=7;
+      int sum = num1+num2+num3;
+      System.out.println(sum);
+  
+
+    }
+    }
+//output is 14
