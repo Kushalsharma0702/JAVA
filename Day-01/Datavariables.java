b4d2ddbcffde78f70e65f610d410055acd8ab69f
Kushal sharma <146510678+Kushalsharma0702@users.noreply.github.com>
2024-07-22 01:03:39 +0530

    Update Datavariables.java

--- Day-01/Datavariables.java
+++ Day-01/Datavariables.java
@@ -1,10 +1,7 @@
 class Datavariables {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        int num1 = 2;
-        int num2 = 3;
-        int num3 = num1+num2;
-        System.out.println(num3);
+    
     }
 }
  
